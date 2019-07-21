debc4a8e4da54104e833bb4808a4ec66b504ed7e
keshav-c <keshav1985@gmail.com>
2019-07-21 14:25:28 +0530

    Add users controller and associated actions and GET views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @name = "This is index action"
+  end
+
+  def new
+    @name = "This is new action"
+  end
+
+  def edit
+    @name = "This is edit action"
+  end
+
+  def show
+    @name = "This is show action"
+  end
+end
--- script/rest_requestor.rb
+++ script/rest_requestor.rb
@@ -2,4 +2,14 @@ require 'rest-client'
 
 url = "http://localhost:3000"
 inpt = gets.chomp
-puts RestClient.get (inpt.empty? ? url : inpt)
+case inpt
+when ""
+  request = url
+when "n"
+  request = "#{ url }/users/new"
+when "e"
+  request = "#{ url }/users/1/edit"
+when "s"
+  request = "#{ url }/users/1"
+end
+puts RestClient.get (request)
